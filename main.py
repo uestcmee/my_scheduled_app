d176c0fb71d49202421dc1a99bcdc6e9d4c07922
zikepeng <zikepeng@outlook.com>
2021-01-21 22:30:27 +0800

    小修改

--- main.py
+++ main.py
@@ -7,33 +7,49 @@ import win32api, win32gui
 ct = win32api.GetConsoleTitle()
 hd = win32gui.FindWindow(0,ct)
 win32gui.ShowWindow(hd,0)
-
+from functools import wraps
+def logged(func):
+    @wraps(func)
+    def with_logging(*args, **kwargs):
+        print (func.__name__)    # 输出 函数名
+        show_notifier('开始运行',func.__doc__)
+        return func(*args, **kwargs)
+    return with_logging
 
 
 def show_notifier( title, text=':)'):
-    toaster.show_toast(title,text,
-                        # icon_path="涨跌.ico",
-                        duration=5)
+    toaster.show_toast(title,text,duration=5)# icon_path="涨跌.ico",)
     return 0
 
+@logged
 def bond_deal_crawler():
+    """
+    同业交易中心债券成交爬虫
+    """
     from bond_deal import fetch_bond_info
-    show_notifier('债券成交','开始运行')
     fetch_bond_info()
-    show_notifier('债券成交','获取完成')
 
+@logged
 def cobalt_price_crawler():
+    """
+    钴等商品价格爬虫
+    """
     from cobalt_price import main_func
-    show_notifier('钴价格爬虫','开始运行')
     main_func()
-    show_notifier('钴价格爬虫','获取完成')
 
 
 
 if __name__ == '__main__':
     show_notifier('定期启动程序', '已开始运行')
+    # # 一个愚蠢的方式来工作日启动
+    # schedule.every().monday.at("21:30").do(bond_deal_crawler)
+    # schedule.every().tuesday.at("21:30").do(bond_deal_crawler)
+    # schedule.every().wednesday.at("21:30").do(bond_deal_crawler)
+    # schedule.every().thursday.at("21:30").do(bond_deal_crawler)
+    # schedule.every().friday.at("21:30").do(bond_deal_crawler)
+
     schedule.every().day.at("21:30").do(bond_deal_crawler)
-    schedule.every().day.at("21:32").do(cobalt_price_crawler)
+    schedule.every().day.at("21:30:30").do(cobalt_price_crawler)
 
     while True:
         schedule.run_pending()
---
+++ 装饰器测试.py
@@ -0,0 +1,32 @@
+from functools import wraps
+# def logged(func):
+#     @wraps(func)
+#     def with_logging(*args, **kwargs):
+#         print (func.__name__)    # 输出 'f'
+#         print(func.__doc__)  # 输出 'does some math'
+#         return func(*args, **kwargs)
+#     return with_logging
+#
+# @logged
+# def f(x):
+#    """does some math"""
+#    return x + x * x
+#
+# print(f(2))
+
+# 类装饰器怎么用wraps呢
+class Foo(object):
+    def __init__(self, func):
+        self._func = func
+
+    def __call__(self):
+        print ('class decorator runing')
+        print(self._func.__doc__)  # 输出 'does some math'
+        self._func()
+        print ('class decorator ending')
+
+@Foo
+def bar():
+    print ('bar')
+
+bar()
